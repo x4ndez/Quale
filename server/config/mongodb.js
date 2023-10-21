a409ca97a67c9fb880f8b6c31e2fab8cb6ec86b4
x.ande <ananfro@live.com>
2023-10-21 14:34:17 +1100

    changed mongodb dbname

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/newDB");
+mongoose.connect("mongodb://127.0.0.1:27017/quale");
 
 module.exports = mongoose.connection;
